3e6a135e065a148822d24c8345348fe723877c34
Danah <danalmaghamsi@gmail.com>
2025-05-15 00:36:23 +0000

    The start session scrept has been added by danah

---
+++ start_session.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -f session.tmp ] ; then #check if there is already session activated
+  echo "Alert:session is currently active"
+  exit 1 #stop the execution 
+fi
+
+#start time for session
+Start_Time=$(date '+%Y-%m-%d %H:%:%S')
+echo "START:$Start_Time" > session.tmp
+echo "The session have started: $Start_Time" 

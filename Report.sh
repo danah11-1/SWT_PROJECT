7df417e1cfd881c0528e07c3e0d2903cfdf0410c
Joudy0-A <mooonn8926@gmail.com>
2025-05-16 19:04:39 +0300

    Update Report.sh

--- Report.sh
+++ Report.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-File=
+File="logs/session_log.txt"
+if[ ! -f "$File" ]; then
+ echo "NO session log found"
+ exit 1
+fi
+
+#Total number of sessions
+Total_Session=$(wc -l < "File")
+echo "Total Session: $Total_Session"
+
+#Total duration in seconds
+Total_Duration=0
+Longest_
+ 

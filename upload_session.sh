3f135371b6c7ba14c35465ad5eb37599785ceb87
Danah <danalmaghamsi@gmail.com>
2025-05-17 00:15:29 +0000

    upload session

--- upload_session.sh
+++ upload_session.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-git add logs/session.log.txt
+Current_Branch=$(git branch --show-current)
+git add logs/session_log.txt
 git commit -m "update the session: $(date '+%Y-%m-%d %H:%M:%S')"
-git push origin main
+git push origin "$Current_Branch"

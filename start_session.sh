faff0e5bff8a742845a05fb907cb0f705f4b05b0
Danah <danalmaghamsi@gmail.com>
2025-05-19 00:15:32 +0000

    add comments

--- start_session.sh
+++ start_session.sh
@@ -10,8 +10,10 @@ Start_Time=$(date '+%Y-%m-%d %H:%M:%S')
 echo "START:$Start_Time" > session.tmp
 echo "The session have started: $Start_Time" 
 
+#start background timer for 45 minutes
 (
  sleep 2700
+#check if the session still active
  if [ -f session.tmp ]; then
  echo "Alert:it's been 45 minutes since the session, move aruond a bit and take a break"
  fi
